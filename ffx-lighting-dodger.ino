5964d0b06cee5fd555905d7d9582e5e7e7ffc674
Daniele Sabre <daniele.sabre@gmail.com>
2022-06-27 21:09:47 +0200

    Update ffx-lighting-dodger.ino

--- ffx-lighting-dodger.ino
+++ ffx-lighting-dodger.ino
@@ -11,7 +11,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo servo;
 
-int lightVal = 0, dodged = 0, maxLight = 0;
+int lightVal = 0, dodged = 0, maxLight = 0, minLight = 9999;
 
 // this value is dependent on the photoresistor and the brightness of your TV
 // you will probably need to test different values here
@@ -23,52 +23,70 @@ int startDegs = 180;
 int pressDegs = 15;
 
 void updateLCD(){
-	lcd.clear();
+    String spaces1 = "";
+    for(int i = 0; i < 8 - String(dodged).length() - String(lightVal).length(); i++){
+        spaces1 = spaces1 + " ";
+    }
 
-	lcd.home();
-	lcd.print("Dodged: ");
-	lcd.print(dodged);
+    String spaces2 = "";
+    for(int i = 0; i < 4 - String(minLight).length(); i++){
+        spaces2 = spaces2 + " ";
+    }
+    
+    lcd.clear();
 
-	lcd.setCursor(0, 1);
-	lcd.print("VL: ");
-	lcd.print(lightVal);
-	lcd.print(" VM: ");
-	lcd.print(maxLight);
+    lcd.home();
+    lcd.print("LD: ");
+    lcd.print(dodged);
+    lcd.print(spaces1 + " L: ");
+    lcd.print(lightVal);
+
+    lcd.setCursor(0, 1);
+    lcd.print("Lm: ");
+    lcd.print(minLight);
+    lcd.print(spaces2 + " LM: ");
+    lcd.print(maxLight);
 }
 
 void setup(){
-	Serial.begin(9600);
-	Wire.begin();
+    Serial.begin(9600);
+    Wire.begin();
 
-	pinMode(photoPin, INPUT);
+    pinMode(photoPin, INPUT);
 
-	servo.attach(servoPin);
-	servo.write(startDegs);
+    servo.attach(servoPin);
+    servo.write(startDegs);
 
-	lcd.begin(16, 2);
-	updateLCD();
+    lcd.begin(16, 2);
+    updateLCD();
 }
 
 void loop(){
-	lightVal = analogRead(photoPin);
+    lightVal = analogRead(photoPin);
 
-	// print light value in serial
-	Serial.println(lightVal);
+    // print light value in serial
+    Serial.println(lightVal);
   
-	if(lightVal > maxLight){
-		maxLight = lightVal;
-      		updateLCD();
-	}
+    if(lightVal > maxLight){
+        maxLight = lightVal;
+        updateLCD();
+    }
 
-	if (lightVal > threshold){
-		delay(50);
-		servo.write(startDegs - pressDegs);
-		delay(200);
-		servo.write(startDegs);
-		dodged++;
-		updateLCD();
-		delay(990); // "debouncing"
-	}
+    if(lightVal < minLight){
+        minLight = lightVal;
+        updateLCD();
+    }
 
-	delay(10);
+    if (lightVal > threshold){
+        minLight = 9999;
+        delay(50);
+        servo.write(startDegs - pressDegs);
+        delay(200);
+        servo.write(startDegs);
+        dodged++;
+        updateLCD();
+        delay(990); // "debouncing"
+    }
+
+    delay(10);
 }
